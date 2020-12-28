a72c806bd26239172506581392a57688c78e54e6
Paul Moreland <pamo80@pamoserver.home>
2020-12-28 15:51:52 +0100

    First commit

---
+++ template/README.md
@@ -0,0 +1,3 @@
+# Pamosystems API
+
+This API is created by Paul Moreland, Pamosystems.
---
+++ template/functions/responses/404.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = async (ctx) => {
+  return ctx.notFound('Tyvärr, denna sidan finns inte.');
+};
